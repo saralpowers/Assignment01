3d15a02a7fe158186c2e3427bc653365ee16d5bc
saralpowers <saralpowers@gmail.com>
2021-02-14 16:55:40 -0800

    second change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,75 @@
-//STEP 1
-//STEP 2
-//STEP 3
+//COMM 644 Assignment 1
+//Sara Powers
+//2/17/2021
+
+
+//STEP 1: camelCaseNotation
+
+//  let someMonth;
+//  function theMonth();
+//  let currentMonth;
+//  let summerMonth;
+//  let myLibraryFunction;
+
+
+//STEP 2: expression examples
+
+//  numeric literal expression: 3.14
+//  string literal expression: "Hello World"
+//  Boolean literal expression: true
+//  null literal expression: null
+
+
+//STEP 3: complex/variable expressions
+
+//  let milesPerGallon = 240 / 60;
+//  let hoursInWeek = 7 * 24;
+
+
+//STEP 4: declare identifiers
+
+//  let sFirstName;
+//  let sLastName;
+//  let sAddress;
+//  let sCity;
+//  let sState;
+//  let iZipCode;
+//  let iYourAge;
+//  let sReferralSource;
+//  let bLetContact;
+
+
+//STEP 5: declare and assign variables
+
+//  let sFirstName = "Sara";
+//  let sLastName = "Powers";
+//  let bLetContact = false;
+
+
+//STEP 6: coercion example of a string and number
+
+//let date;
+//date = "February " + 17;
+//window.console.log(date);
+//window.console.log(typeof date);
+
+
+//STEP 7: coercion examples with Boolean type
+
+//adding a string and a Boolean 
+//let hwStatus;
+//hwStatus = "completed " + false;
+//window.console.log(hwStatus);
+//window.console.log(typeof hwStatus);
+
+//adding a number and a Boolean
+//let theYear;
+//theYear = 2021 + true;
+//window.console.log(theYear);
+//window.console.log(typeof theYear);
+
+
+
+
+
+
